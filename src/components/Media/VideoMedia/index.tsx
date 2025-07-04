71fe528b8c30ed65badbf48c43c3cc3e507d45ed
Dwi-Wahyu <dwiwahyuilahi123@gmail.com>
2025-07-05 00:15:57 +0800

    fix: run port

--- src/components/Media/VideoMedia/index.tsx
+++ src/components/Media/VideoMedia/index.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useRef } from 'react'
 
 import type { Props as MediaProps } from '../types'
 import { getMediaUrl } from '@/utilities/getMediaUrl'
